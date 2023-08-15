e9d685c9cf73656736fd0098aec47371e29a3f44
Tanay Bhomia <bhomia123456789@gmail.com>
2023-08-15 19:20:17 +0530

    Removed everything

--- app/views.py
+++
@@ -1,9 +0,0 @@
-# Navigation for the user 
-from flask import Blueprint
-
-views = Blueprint('view,__name__')
-
-# whenever the user goes to the root directory this function is called 
-@views.route('/')
-def home():
-    pass
